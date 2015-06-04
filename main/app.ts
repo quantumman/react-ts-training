948a1e1cb2831dc1ee1e571c3575a19f696b2e53
Syoudai Yokoyama <s-yokoyama@timedia.co.jp>
2015-06-04 15:49:59 +0900

    Show CommentBox

--- main/app.ts
+++ main/app.ts
@@ -2,3 +2,17 @@
 ///<reference path="../typings/alloy-editor.d.ts" />
 
 import React = require('react/addons')
+
+class CommentBox extends React.Component<{}, {}> {
+    render() {
+        return React.jsx(`
+                <div className="commentBox">
+                         Hello, world! I am a CommentBox.
+                </div>
+        `)
+    }
+}
+React.render(
+    React.createElement(CommentBox),
+    document.getElementById('content')
+)
