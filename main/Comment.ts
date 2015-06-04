2ef39f9dd324baed86a2edd22a51a6297b7fe4c2
Syoudai Yokoyama <s-yokoyama@timedia.co.jp>
2015-06-04 18:24:54 +0900

    Add Comment

--- main/Comment.ts
+++ main/Comment.ts
@@ -8,14 +8,14 @@ interface CommentProps extends React.Props<void> {
 
 class Comment extends React.Component<CommentProps, void> {
     render() {
-	return React.jsx(`
-		<div className="comment">
-			<h2 className="commentAuthor">
-				{this.props.author}
-			</h2>
-			{this.props.children}
-		</div>
-	`)
+        return React.jsx(`
+                <div className="comment">
+                        <h2 className="commentAuthor">
+                                {this.props.author}
+                        </h2>
+                        {this.props.children}
+                </div>
+        `)
     }
 }
 
