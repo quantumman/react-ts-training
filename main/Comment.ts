45c6d00862809e95d7c50ecef4e5565dbfc4bfb6
Syoudai Yokoyama <s-yokoyama@timedia.co.jp>
2015-06-04 17:24:31 +0900

    Add Comment

---
+++ main/Comment.ts
@@ -0,0 +1,23 @@
+///<reference path="../typings/tsd.d.ts" />
+
+import React = require('react/addons')
+
+interface CommentProps extends React.Props<void> {
+    author: string;
+}
+
+class Comment extends React.Component<CommentProps, void> {
+    render() {
+	return React.jsx(`
+		<div className="comment">
+			<h2 className="commentAuthor">
+				{this.props.author}
+			</h2>
+			{this.props.children}
+		</div>
+	`)
+    }
+}
+
+
+export = Comment;
