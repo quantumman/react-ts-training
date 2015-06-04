4713e9c1d737dc6fb77676fe6db50145ad06b7de
Syoudai Yokoyama <s-yokoyama@timedia.co.jp>
2015-06-04 15:53:30 +0900

    Add CommnetList

---
+++ main/CommentList.ts
@@ -0,0 +1,15 @@
+///<reference path="../typings/tsd.d.ts" />
+
+import React = require('react/addons')
+
+class CommentList extends React.Component<{}, {}> {
+    render() {
+	return React.jsx(`
+		<div className="commnetList">
+			Hello, world! I am a CommentList.
+		</div>
+	`)
+    }
+}
+
+export CommentList
