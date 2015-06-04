f1055a6a0f918b0bb0db66f6fc72b5009465e7ef
Syoudai Yokoyama <s-yokoyama@timedia.co.jp>
2015-06-04 18:24:31 +0900

    CommentList: Show Comments

--- main/CommentList.ts
+++ main/CommentList.ts
@@ -1,12 +1,15 @@
 ///<reference path="../typings/tsd.d.ts" />
+///<reference path="./Comment.ts" />
 
 import React = require('react/addons')
+import Comment = require('./Comment')
 
 class CommentList extends React.Component<{}, {}> {
     render() {
         return React.jsx(`
                 <div className="commnetList">
-                        Hello, world! I am a CommentList.
+                        <Comment author="Pete Hunt">This is one comment</Comment>
+                        <Comment author="Jordan Walke">This is *author* comment</Comment>
                 </div>
         `)
     }
