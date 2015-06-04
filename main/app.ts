db5101b7603d1de7602a67f020366990bdde58b5
Syoudai Yokoyama <s-yokoyama@timedia.co.jp>
2015-06-04 17:10:22 +0900

    Show comments

--- main/app.ts
+++ main/app.ts
@@ -1,13 +1,18 @@
 ///<reference path="../typings/tsd.d.ts" />
-///<reference path="../typings/alloy-editor.d.ts" />
+///<reference path="./CommentList.ts" />
+///<reference path="./CommentForm.ts" />
 
 import React = require('react/addons')
+import CommentList = require('./CommentList')
+import CommentForm = require('./CommentForm')
 
 class CommentBox extends React.Component<{}, {}> {
     render() {
         return React.jsx(`
                 <div className="commentBox">
-                         Hello, world! I am a CommentBox.
+                        <h1>Comments</h1>
+                        <CommentList />
+                        <CommentForm />
                 </div>
         `)
     }
