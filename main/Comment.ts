ea12f490b3967f775e2234f1ca50c21ffa56333f
Syoudai Yokoyama <s-yokoyama@timedia.co.jp>
2015-06-04 18:35:27 +0900

    Comment: Markup comment

--- main/Comment.ts
+++ main/Comment.ts
@@ -1,6 +1,7 @@
 ///<reference path="../typings/tsd.d.ts" />
 
 import React = require('react/addons')
+import Marked = require('marked')
 
 interface CommentProps extends React.Props<void> {
     author: string;
@@ -8,12 +9,13 @@ interface CommentProps extends React.Props<void> {
 
 class Comment extends React.Component<CommentProps, void> {
     render() {
+        var rawMarkup = Marked(this.props.children.toString(), {sanitize: true})
         return React.jsx(`
                 <div className="comment">
                         <h2 className="commentAuthor">
                                 {this.props.author}
                         </h2>
-                        {this.props.children}
+                        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
                 </div>
         `)
     }
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,2 +1,3 @@
 /// <reference path="react/react.d.ts" />
 /// <reference path="react/react-addons.d.ts" />
+/// <reference path="marked/marked.d.ts" />
