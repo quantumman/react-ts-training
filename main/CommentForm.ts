97fe4abbbb5c6cd03d1cf53e96346d10c8b0aa5f
Syoudai Yokoyama <s-yokoyama@timedia.co.jp>
2015-06-05 19:58:20 +0900

    CommentForm: Handle comment post

--- main/CommentForm.ts
+++ main/CommentForm.ts
@@ -3,11 +3,24 @@
 import React = require('react/addons')
 
 class CommentForm extends React.Component<{}, void> {
+    handleSubmit(event) {
+        event.preventDefault()
+        var author = React.findDOMNode(this.refs["author"])["value"].trim()
+        var text = React.findDOMNode(this.refs["text"])["value"].trim()
+        if (!text || !author) {
+            return;
+        }
+        // TODO: send request to the server
+        React.findDOMNode(this.refs["author"])["value"] = '';
+        React.findDOMNode(this.refs["text"])["value"] = '';
+        return;
+    }
+
     render(): React.ReactElement<{}> {
         return React.jsx(`
-                <form className="commentForm">
-                        <input type="text" placeholder="Your name" />
-                        <input type="text" placeholder="Say something..." />
+                <form className="commentForm" onSubmit={this.handleSubmit.bind(this)}>
+                        <input type="text" placeholder="Your name" ref="author" />
+                        <input type="text" placeholder="Say something..." ref="text" />
                         <input type="submit" value="Post" />
                 </form>
         `);
